67eb45f9dd15e7bfeeaea9fd2d0f81e070b7a41f
Martzz-a <maritzafidelyah@gmail.com>
2026-02-23 15:31:03 +0700

    first commit

---
+++ coba.cpp
@@ -0,0 +1,15 @@
+//contoh library
+#include <iostream>
+using namespace std;
+
+//variable global
+string name;
+
+int main(){ //program utama
+    int age; //variable local
+    age = 19;
+
+    cout << "Hello World" << endl;
+    cout << 99 << endl;
+    cout << age;
+}
