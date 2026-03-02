8ad2aecd2478ef9bae1e91b565044f26588ec8d6
Martzz-a <maritzafidelyah@gmail.com>
2026-03-02 15:23:42 +0700

    Menambahkan Prosedur Input

--- fungsiParameter.cpp
+++ fungsiParameter.cpp
@@ -4,3 +4,12 @@ using namespace std;
 
 //mendeklarasikan variabel global
 int panjang, lebar;
+
+//implementasi prosedur input
+void input(){
+    cout << "Masukkan panjang: ";
+    cin >> panjang;
+
+    cout << "Masukkan lebar: ";
+    cin >> lebar;
+}
